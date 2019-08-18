d26d51ea7ee99c2478dada990306f9f163f33236
oceancx <oceancx@gmail.com>
2019-08-18 23:10:57 +0800

    add types

--- include/define_types.h
+++ include/define_types.h
@@ -64,3 +64,76 @@ struct TGA_FILE_HEADER
 #endif
 
 #define _T(x) std::string(x)
+
+
+
+class Direction
+{
+public:
+	enum Dir
+	{
+		N = 6,
+		S = 4,
+		W = 5,
+		E = 7,
+		N_E = 3,
+		N_W = 2,
+		S_E = 0,
+		S_W = 1,
+	};
+};
+enum ActionEnums
+{
+	ACTION_IDLE = 0,
+	ACTION_WALK,
+	ACTION_SIT,
+	ACTION_ANGRY,
+	ACTION_SAYHI,
+	ACTION_DANCE,
+	ACTION_SALUTE,
+	ACTION_CLPS,
+	ACTION_CRY,
+	ACTION_BATIDLE,
+	ACTION_ATTACK,
+	ACTION_CAST,
+	ACTION_BEHIT,
+	ACTION_RUNTO,
+	ACTION_RUNBACK,
+	ACTION_DEFEND,
+	ACTION_COUNT
+};
+
+enum EActorType
+{
+	ACTOR_TYPE_DEFAULT = 0,
+	ACTOR_TYPE_PLAYER,
+	ACTOR_TYPE_PET,
+	ACTOR_TYPE_NPC
+};
+
+#define AddonWDF      0x10
+#define AtomWDF       0x20
+#define ChatWDF       0x30
+#define FireworkWDF   0x40
+#define GoodsWDF      0x50
+#define ItemWDF       0x60
+#define MagicWDF      0x70
+#define MapaniWDF     0x80
+#define MhimageWDF    0x90
+#define MiscWDF       0xa0
+#define MusicWDF      0xb0
+#define SceneWDF      0xc0
+#define ShapeWDF      0xd0
+#define ShapeWD1      0xd1
+#define ShapeWD2      0xd2
+#define ShapeWD3      0xd3
+#define ShapeWD4      0xd4
+#define ShapeWD5      0xd5
+#define ShapeWD6      0xd6
+#define ShapeWD7      0xd7
+#define SmapWDF       0xe0
+#define SoundWDF      0xf0
+#define StockWDF      0x100
+#define WaddonWDF     0x110
+#define WzifeWDF      0x120
+#define WzifeWD1      0x121
